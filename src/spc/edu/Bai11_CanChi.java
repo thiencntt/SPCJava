1feee976514195f0e1a4510e60f157be438035e2
thiencntt <nguyenchithien@gmail.com>
2024-10-14 20:24:55 +0700

    Them can chi

---
+++ src/spc/edu/Bai11_CanChi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SPC
+ */
+public class Bai11_CanChi {
+    public static void main(String[] args) {
+        System.out.println("Chuong trinh tinh can chi");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so: ");
+        // String hoten = sc.next();
+        int so = sc.nextInt();
+        int giaithua = 1;
+        
+        for (int i = 1; i <= so; i++) {
+            giaithua = giaithua * i;
+        }        
+        
+        System.out.printf("%d! = %d",so,giaithua);
+        System.out.println();
+        
+        
+    }
+}
